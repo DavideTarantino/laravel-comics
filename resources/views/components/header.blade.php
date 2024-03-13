62b4ce92aaec026cdcd724525a2d2fb2ade6adb9
DavideTarantino <davidetarantino2005@gmail.com>
2024-03-13 15:22:02 +0100

    inserimento della nav-bar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,49 @@
-<h1>Componente Header</h1>
-<nav>
-    <ul>
-        <li><a href="{{ route('homePage') }}">Home</a></li>
-        <li><a href="{{ route('aboutPage') }}">About</a></li>
-    </ul>
-</nav>
+<main>
+    <figure>
+        <img src="{{ Vite::asset('resources/img/logo-sm.png') }}" alt="logo">
+    </figure>
+        <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+        </ul>
+        <input type="text" placeholder="search">
+</main>
+
+<style lang="scss" scoped>
+    main{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 70%;
+        margin: 0 auto;
+        padding: 1%;
+
+        img{
+            width: 80%;
+        }
+
+        ul{
+            display: flex;
+            gap: 30px;
+            
+            li{
+                text-transform: uppercase;
+                font-family: sans-serif;
+                list-style-type: none;
+                font-size: 80%;
+            }
+        }
+    }
+
+    .active{
+        color: blue;
+    }
+</style>
--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 
-    $greeting = 'Hello World';
-
-    $data = [
-        [
-            'name' => 'Fabio'
-        ],
-        [
-            'name' => 'Mario'
-        ],
-        [
-            'name' => 'Sara'
-        ],
-        [
-            'name' => 'Alex'
-        ],
-    ];
-
-
-    return view('welcome', compact('data', 'greeting'));
-})->name('homePage');
-
-Route::get('/about', function () {
-
-    $lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aut excepturi deleniti cupiditate deserunt sint reprehenderit quibusdam tempore voluptas quis, nisi id voluptatibus quaerat repellat doloremque eum eos laborum eaque.';
-
-    return view('about', compact('lorem'));
-})->name('aboutPage');
+Route::get('/')
